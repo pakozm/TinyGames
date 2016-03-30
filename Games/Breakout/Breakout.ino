d0d16fbff095f7dd0d62d243a075ae473891fb16
pakozm <pakozm@gmail.com>
2016-03-30 19:11:37 +0200

    code refactored

--- Games/Breakout/Breakout.ino
+++ Games/Breakout/Breakout.ino
@@ -13,6 +13,7 @@
 */
 #include <EEPROM.h>
 #include "font6x8.h"
+#include "ssd1306.h"
 #include <avr/sleep.h>
 #include <avr/interrupt.h> // needed for the additional interrupt
 
@@ -31,6 +32,7 @@ ISR(PCINT0_vect){ // PB0 pin button interrupt
    if (player >0){player--;} 
    return;
 }
+
 void playerInc(){ // PB2 pin button interrupt
   if (player <128-platformWidth){player++;}
 }
@@ -265,121 +267,6 @@ void sendBlock(boolean fill){
 }
 void beep(int bCount,int bDelay){
   for (int i = 0; i<=bCount; i++){digitalWrite(1,HIGH);for(int i2=0; i2<bDelay; i2++){__asm__("nop\n\t");}digitalWrite(1,LOW);for(int i2=0; i2<bDelay; i2++){__asm__("nop\n\t");}}
-}
-#define DIGITAL_WRITE_HIGH(PORT) PORTB |= (1 << PORT)
-#define DIGITAL_WRITE_LOW(PORT) PORTB &= ~(1 << PORT)
-
-// Some code based on "IIC_wtihout_ACK" by http://www.14blog.com/archives/1358
-#ifndef SSD1306XLED_H
-#define SSD1306XLED_H
-// ---------------------	// Vcc,	Pin 1 on SSD1306 Board
-// ---------------------	// GND,	Pin 2 on SSD1306 Board
-#ifndef SSD1306_SCL
-#define SSD1306_SCL		PB3	// SCL,	Pin 3 on SSD1306 Board
-#endif
-#ifndef SSD1306_SDA
-#define SSD1306_SDA		PB4	// SDA,	Pin 4 on SSD1306 Board
-#endif
-#ifndef SSD1306_SA
-#define SSD1306_SA		0x78	// Slave address
-#endif
-// ----------------------------------------------------------------------------
-void ssd1306_init(void);
-void ssd1306_xfer_start(void);
-void ssd1306_xfer_stop(void);
-void ssd1306_send_byte(uint8_t byte);
-void ssd1306_send_command(uint8_t command);
-void ssd1306_send_data_start(void);
-void ssd1306_send_data_stop(void);
-void ssd1306_setpos(uint8_t x, uint8_t y);
-void ssd1306_fillscreen(uint8_t fill_Data);
-void ssd1306_char_f6x8(uint8_t x, uint8_t y, const char ch[]);
-//void ssd1306_char_f8x16(uint8_t x, uint8_t y,const char ch[]);
-//void ssd1306_char_f16x16(uint8_t x, uint8_t y, uint8_t N);
-void ssd1306_draw_bmp(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t bitmap[]);
-// ----------------------------------------------------------------------------
-#endif
-void ssd1306_init(void){
-	DDRB |= (1 << SSD1306_SDA);	// Set port as output
-	DDRB |= (1 << SSD1306_SCL);	// Set port as output
-
-	ssd1306_send_command(0xAE); // display off
-	ssd1306_send_command(0x00); // Set Memory Addressing Mode
-	ssd1306_send_command(0x10); // 00,Horizontal Addressing Mode;01,Vertical Addressing Mode;10,Page Addressing Mode (RESET);11,Invalid
-	ssd1306_send_command(0x40); // Set Page Start Address for Page Addressing Mode,0-7
-	ssd1306_send_command(0x81); // Set COM Output Scan Direction
-	ssd1306_send_command(0xCF); // ---set low column address
-	ssd1306_send_command(0xA1); // ---set high column address
-	ssd1306_send_command(0xC8); // --set start line address
-	ssd1306_send_command(0xA6); // --set contrast control register
-	ssd1306_send_command(0xA8);
-	ssd1306_send_command(0x3F); // --set segment re-map 0 to 127
-	ssd1306_send_command(0xD3); // --set normal display
-	ssd1306_send_command(0x00); // --set multiplex ratio(1 to 64)
-	ssd1306_send_command(0xD5); // 
-	ssd1306_send_command(0x80); // 0xa4,Output follows RAM content;0xa5,Output ignores RAM content
-	ssd1306_send_command(0xD9); // -set display offset
-	ssd1306_send_command(0xF1); // -not offset
-	ssd1306_send_command(0xDA); // --set display clock divide ratio/oscillator frequency
-	ssd1306_send_command(0x12); // --set divide ratio
-	ssd1306_send_command(0xDB); // --set pre-charge period
-	ssd1306_send_command(0x40); // 
-	ssd1306_send_command(0x20); // --set com pins hardware configuration
-	ssd1306_send_command(0x02);
-	ssd1306_send_command(0x8D); // --set vcomh
-	ssd1306_send_command(0x14); // 0x20,0.77xVcc
-	ssd1306_send_command(0xA4); // --set DC-DC enable
-	ssd1306_send_command(0xA6); // 
-	ssd1306_send_command(0xAF); // --turn on oled panel 
-}
-
-void ssd1306_xfer_start(void){
-	DIGITAL_WRITE_HIGH(SSD1306_SCL);	// Set to HIGH
-	DIGITAL_WRITE_HIGH(SSD1306_SDA);	// Set to HIGH
-	DIGITAL_WRITE_LOW(SSD1306_SDA);		// Set to LOW
-	DIGITAL_WRITE_LOW(SSD1306_SCL);		// Set to LOW
-}
-
-void ssd1306_xfer_stop(void){
-	DIGITAL_WRITE_LOW(SSD1306_SCL);		// Set to LOW
-	DIGITAL_WRITE_LOW(SSD1306_SDA);		// Set to LOW
-	DIGITAL_WRITE_HIGH(SSD1306_SCL);	// Set to HIGH
-	DIGITAL_WRITE_HIGH(SSD1306_SDA);	// Set to HIGH
-}
-
-void ssd1306_send_byte(uint8_t byte){
-	uint8_t i;
-	for(i=0; i<8; i++)
-	{
-		if((byte << i) & 0x80) 			DIGITAL_WRITE_HIGH(SSD1306_SDA); 		else 			DIGITAL_WRITE_LOW(SSD1306_SDA); 		 		DIGITAL_WRITE_HIGH(SSD1306_SCL); 		DIGITAL_WRITE_LOW(SSD1306_SCL); 	} 	DIGITAL_WRITE_HIGH(SSD1306_SDA); 	DIGITAL_WRITE_HIGH(SSD1306_SCL); 	DIGITAL_WRITE_LOW(SSD1306_SCL); } void ssd1306_send_command(uint8_t command){ 	ssd1306_xfer_start(); 	ssd1306_send_byte(SSD1306_SA);  // Slave address, SA0=0 	ssd1306_send_byte(0x00);	// write command 	ssd1306_send_byte(command); 	ssd1306_xfer_stop(); } void ssd1306_send_data_start(void){ 	ssd1306_xfer_start(); 	ssd1306_send_byte(SSD1306_SA); 	ssd1306_send_byte(0x40);	//write data } void ssd1306_send_data_stop(void){ 	ssd1306_xfer_stop(); } void ssd1306_setpos(uint8_t x, uint8_t y) { 	ssd1306_xfer_start(); 	ssd1306_send_byte(SSD1306_SA);  //Slave address,SA0=0 	ssd1306_send_byte(0x00);	//write command 	ssd1306_send_byte(0xb0+y); 	ssd1306_send_byte(((x&0xf0)>>4)|0x10); // |0x10
-	ssd1306_send_byte((x&0x0f)|0x01); // |0x01
-
-	ssd1306_xfer_stop();
-}
-
-void ssd1306_fillscreen(uint8_t fill_Data){
-	uint8_t m,n;
-	for(m=0;m<8;m++)
-	{
-		ssd1306_send_command(0xb0+m);	//page0-page1
-		ssd1306_send_command(0x00);		//low column start address
-		ssd1306_send_command(0x10);		//high column start address
-		ssd1306_send_data_start();
-		for(n=0;n<128;n++) 		{ 			ssd1306_send_byte(fill_Data); 		} 		ssd1306_send_data_stop(); 	} } void ssd1306_char_f6x8(uint8_t x, uint8_t y, const char ch[]){ 	uint8_t c,i,j=0; 	while(ch[j] != '\0') 	{ 		c = ch[j] - 32; 		if(x>126)
-		{
-			x=0;
-			y++;
-		}
-		ssd1306_setpos(x,y);
-		ssd1306_send_data_start();
-		for(i=0;i<6;i++)
-		{
-			ssd1306_send_byte(pgm_read_byte(&ssd1306xled_font6x8[c*6+i]));
-		}
-		ssd1306_send_data_stop();
-		x += 6;
-		j++;
-	}
 }
 
 // Routines to set and clear bits (used in the sleep code)
---
+++ Games/libraries/ssd1306xled/ssd1306.h
@@ -0,0 +1,30 @@
+// Some code based on "IIC_wtihout_ACK" by http://www.14blog.com/archives/1358
+#ifndef SSD1306XLED_H
+#define SSD1306XLED_H
+// ---------------------  // Vcc, Pin 1 on SSD1306 Board
+// ---------------------  // GND, Pin 2 on SSD1306 Board
+#ifndef SSD1306_SCL
+#define SSD1306_SCL   PB3 // SCL, Pin 3 on SSD1306 Board
+#endif
+#ifndef SSD1306_SDA
+#define SSD1306_SDA   PB4 // SDA, Pin 4 on SSD1306 Board
+#endif
+#ifndef SSD1306_SA
+#define SSD1306_SA    0x78  // Slave address
+#endif
+// ----------------------------------------------------------------------------
+void ssd1306_init(void);
+void ssd1306_xfer_start(void);
+void ssd1306_xfer_stop(void);
+void ssd1306_send_byte(uint8_t byte);
+void ssd1306_send_command(uint8_t command);
+void ssd1306_send_data_start(void);
+void ssd1306_send_data_stop(void);
+void ssd1306_setpos(uint8_t x, uint8_t y);
+void ssd1306_fillscreen(uint8_t fill_Data);
+void ssd1306_char_f6x8(uint8_t x, uint8_t y, const char ch[]);
+//void ssd1306_char_f8x16(uint8_t x, uint8_t y,const char ch[]);
+//void ssd1306_char_f16x16(uint8_t x, uint8_t y, uint8_t N);
+void ssd1306_draw_bmp(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t bitmap[]);
+// ----------------------------------------------------------------------------
+#endif // SSD1306XLED_H
